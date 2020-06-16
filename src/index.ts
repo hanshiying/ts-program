361a12b93b8f9a3d43ce612db0072c4e8147961b
hanshiying <hanshi_ying@163.com>
2020-06-17 00:11:04 +0800

    demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { sum } from "./common"
+// import { sum } from "./common"
 
 const message = 'hello typescritp!!'
-const sumVar = sum(1, 3)
-console.log(sumVar)
 document.querySelectorAll('#app')[0].innerHTML = message
-console.log(sum(4, 25))
+class animal {
+    constructor(public name: string) {
+        this.name = name
+    }
+    call1() {
+        return this
+    }
+    call2() {
+        return this
+    }
+}
+class cat extends animal {
+    speak() {
+        console.log(`cat ${this.name} is speaking`)
+        return this
+    }
+}
+new cat('33').call1().call2().speak()
